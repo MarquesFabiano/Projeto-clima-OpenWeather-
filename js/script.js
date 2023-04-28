010d6d7a4166d9cf955945b3fab6ccc338eec782
MarquesFabiano <111817663+MarquesFabiano@users.noreply.github.com>
2023-04-28 05:37:11 -0300

    Iniciando o JS

--- js/script.js
+++ js/script.js
@@ -1,2 +1,29 @@
-// ff1fa786020cf7448474242c208b61a6  CHAVE API
-// https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=ff1fa786020cf7448474242c208b61a6
+const apiKey = "f1fa786020cf7448474242c208b61a6";
+const apiCountryURL = "https://flagsapi.com/png/";
+
+const cityInput = document.getElementById("city-input");
+const searchBtn = document.getElementById("search");
+
+const cityElemente = document.getElementById("city");
+const tempElemente = document.getElementById("temperature span");
+const descElemente = document.getElementById("descripttion");
+const weatherIconElemente = document.getElementById("wather-icon");
+const countryElemente = document.getElementById("country");
+const umidityElemente = document.getElementById("umidity span");
+const windElemente = document.getElementById("wind span");
+
+//funções:
+const getWeatherData = async(city) => {}
+
+const showWatherData = (city) => {
+    console.log(city)
+}
+
+// aqui vai os eventos
+searchBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const city = cityInput.value;
+
+    showWatherData(city);
+});
