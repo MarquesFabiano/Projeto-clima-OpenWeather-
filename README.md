50787bcd106e9041046aa18e47b6fd7c2c91af75
MarquesFabiano <111817663+MarquesFabiano@users.noreply.github.com>
2023-04-29 04:24:12 -0300

    final commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Aprendendo um uso de API OpenWeather. Também é utilizado o padrão de nome de variaveis em inglês. 
+Projeto feito em aula com a comunidade "Hora de codar".
--- js/script.js
+++ js/script.js
@@ -1,29 +1,97 @@
-const apiKey = "f1fa786020cf7448474242c208b61a6";
-const apiCountryURL = "https://flagsapi.com/png/";
+const apiKey = "ff1fa786020cf7448474242c208b61a6";
+const apiCountryURL = "https://countryflagsapi.com/png/";
+const apiUnsplash = "https://source.unsplash.com/1600x900/?";
 
-const cityInput = document.getElementById("city-input");
-const searchBtn = document.getElementById("search");
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
 
-const cityElemente = document.getElementById("city");
-const tempElemente = document.getElementById("temperature span");
-const descElemente = document.getElementById("descripttion");
-const weatherIconElemente = document.getElementById("wather-icon");
-const countryElemente = document.getElementById("country");
-const umidityElemente = document.getElementById("umidity span");
-const windElemente = document.getElementById("wind span");
+const cityElement = document.querySelector("#city");
+const tempElement = document.querySelector("#temperature span");
+const descElement = document.querySelector("#description");
+const weatherIconElement = document.querySelector("#weather-icon");
+const countryElement = document.querySelector("#country");
+const umidityElement = document.querySelector("#umidity span");
+const windElement = document.querySelector("#wind span");
 
-//funções:
-const getWeatherData = async(city) => {}
+const weatherContainer = document.querySelector("#weather-data");
 
-const showWatherData = (city) => {
-    console.log(city)
-}
+const errorMessageContainer = document.querySelector("#error-message");
+const loader = document.querySelector("#loader");
 
-// aqui vai os eventos
-searchBtn.addEventListener("click", (e) => {
-    e.preventDefault();
+const suggestionContainer = document.querySelector("#suggestions");
+const suggestionButtons = document.querySelectorAll("#suggestions button");
 
-    const city = cityInput.value;
+// Loader
+const toggleLoader = () => {
+  loader.classList.toggle("hide");
+};
 
-    showWatherData(city);
+const getWeatherData = async (city) => {
+  toggleLoader();
+
+  const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+
+  const res = await fetch(apiWeatherURL);
+  const data = await res.json();
+
+  toggleLoader();
+
+  return data;
+};
+
+// tratamento de erros
+const showErrorMessage = () => {
+  errorMessageContainer.classList.remove("hide");
+};
+
+const hideInformation = () => {
+  errorMessageContainer.classList.add("hide");
+  weatherContainer.classList.add("hide");
+
+  suggestionContainer.classList.add("hide");
+};
+
+const showWeatherData = async (city) => {
+  hideInformation();
+
+  const data = await getWeatherData(city);
+
+  if (data.cod === "404") {
+    showErrorMessage();
+    return;
+  }
+// inserir  os dados do clima nos elementos
+  cityElement.innerText = data.name;
+  tempElement.innerText = parseInt(data.main.temp);
+  descElement.innerText = data.weather[0].description;
+  weatherIconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+  );
+  countryElement.setAttribute("src", apiCountryURL + data.sys.country);
+  umidityElement.innerText = `${data.main.humidity}%`;
+  windElement.innerText = `${data.wind.speed}km/h`;
+
+  //usa api para mudar umagem de fundo
+  document.body.style.backgroundImage = `url("${apiUnsplash + city}")`;
+
+  weatherContainer.classList.remove("hide");
+};
+// previne padrão e chama função 
+searchBtn.addEventListener("click", async (e) => {
+  e.preventDefault();
+
+  const city = cityInput.value;
+
+  showWeatherData(city);
 });
+
+// Para enviar o calor ao apertar enter!
+cityInput.addEventListener("keyup", (e) => {
+  if (e.code === "Enter") {
+    const city = e.target.value;
+
+    showWeatherData(city);
+  }
+});
+
